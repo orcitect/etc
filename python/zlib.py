9c594ea7dde028bb824dfde2b27d7e0e8a4a44ee
Oscar Petersén <orcitect@runbox.com>
2019-02-21 23:23:39 +0100

    rmd

--- python/zlib.py
+++
@@ -1,9 +0,0 @@
-import zlib
-
-data = "Hello, World!" * 100
-checksum = zlib.crc32(data)
-
-compressed = zlib.compress(data)
-decompressed = zlib.decompress(compressed)
-
-print zlib.crc32(decompressed) == checksum
