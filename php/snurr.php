0ff00ad6a554cbc7327a229e1a3a646c887b9350
Oscar Petersén <opetersen@runbox.com>
2019-07-30 13:54:55 +0200

    Update snurr.php

--- php/snurr.php
+++ php/snurr.php
@@ -4,7 +4,7 @@ $str="abcdefghijklmnopqrstuvwxyz@AB%CDEFGHIJKLMNOPQRST!UVWXYZ0123456789@";
 
 for ($i=0;$i<45+rand(0,10);$i++)
 {
-	echo $str[rand(0,strlen($str)-1)];
+    echo $str[rand(0,strlen($str)-1)];
 }
 
 ?>
